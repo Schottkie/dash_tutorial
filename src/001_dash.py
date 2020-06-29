7035b1dbdefbe780244c8710fbb50aabf63eefda
schottkie <tomjungermann@gmail.com>
2020-06-29 19:48:58 +0200

    001_first dash app using only dash_html_component

--- src/001_dash.py
+++ src/001_dash.py
@@ -1,5 +1,10 @@
 import dash
+import dash_html_components as html
 
-print('Hello world')
-print('update')
-print('mach was')
+app = dash.Dash(__name__)
+
+# html = <h1>Our first dash app</h1>
+app.layout = html.H1('Our first dash app')
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
